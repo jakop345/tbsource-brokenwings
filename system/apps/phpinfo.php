0bf21faceb184f9891baee3f3a21f478ba3abe08
yeok <yeok@users.sourceforge.net>
2010-03-11 21:30:01 +0000

    added /system/apps/phpinfo.php for development aid. To be removed on for releases!

---
+++ system/apps/phpinfo.php
@@ -0,0 +1,14 @@
+<?PHP
+
+/* TBSource BrokenWings, Copyright (c) 2010 Tbsource project.
+ *
+ * http://sourceforge.net/projects/tbsource/
+ *
+ * todo: Delete this in releases, its a test file for development!
+*/
+
+# call the built in php info function ...!
+phpinfo();
+
+
+?>
